b148a1a34a9427358f99efafc1134f46b32cd8b6
Jenn Yacoubian <jyacoubian@gmail.com>
2023-03-01 16:29:32 -0700

    Install and import react router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import NavBar from '../NavBar/NavBar.js'
+import ArticleList from '../ArticlesList/ArticlesList.js'
+import ArticleDetails from '../ArticleDetails/ArticleDetails.js'
 import "./App.css"
 
 const App = () => {
   return (
     <div>
-      App
+      Hello World!
     </div>
   )
 }
