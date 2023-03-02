65939983cf5d8738f18081aeceebb23bdf08993d
Jenn Yacoubian <jyacoubian@gmail.com>
2023-03-01 19:07:03 -0700

    Add router to app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Routes, Route} from 'react-router-dom'
 import NavBar from '../NavBar/NavBar.js'
-import ArticleList from '../ArticlesList/ArticlesList.js'
+import ArticlesList from '../ArticlesList/ArticlesList.js'
 import ArticleDetails from '../ArticleDetails/ArticleDetails.js'
 import "./App.css"
 
 const App = () => {
+
+  const [articles, setArticles] = useState([])
+
   return (
     <div>
-      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<ArticlesList articles={articles} setArticles={setArticles}/>} />
+        <Route />
+      </Routes>
     </div>
   )
 }
