2e57cf5914070396439c13e55d10041a26bb3270
Jenn Yacoubian <jyacoubian@gmail.com>
2023-03-02 13:30:40 -0700

    Add NavBar render and add category to use effect array to trigger rerender

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useParams} from 'react-router-dom'
 import ArticlesList from '../ArticlesList/ArticlesList.js'
 import ArticleDetails from '../ArticleDetails/ArticleDetails.js'
+import NavBar from '../NavBar/NavBar.js'
 import "./App.css"
 import { getArticles } from '../../APIcalls.js'
 
 const App = () => {
-
   const [articles, setArticles] = useState([])
   const [category, setCategory] = useState("home")
   const [error, setError] = useState("")
@@ -17,13 +17,17 @@ const App = () => {
     .catch((error) => {
       setError('Oops, something went wrong. Please try again later.')
     })
-  }, [])
-  
+  }, [category])
+ 
   return (
+    <div>
+     <NavBar category={category} setCategory={setCategory}/>
       <Routes>
         <Route exact path="/" element={<ArticlesList articles={articles} setArticles={setArticles} />} />
+        <Route path="/:category" element={<ArticlesList articles={articles} />} />
         <Route path="/details/:id" element={<ArticleDetails articles={articles} />} />
       </Routes>
+    </div>
   )
 }
 
